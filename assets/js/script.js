82213bba1b3e667641e429d388e627677ea99190
JosephLindzius <joseph.lindzius@gmail.com>
2019-09-12 11:56:11 +0200

    added api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 document.getElementById("submit").addEventListener("click", function(){
-    axios.get("https://pokeapi.co/api/v2/pokemon/ditto/")
-
-
+    let pokemon = document.getElementById("input").value;
+    axios.get("https://pokeapi.co/api/v2/pokemon/"+ pokemon +"/")
         .then(function(response) {
             console.log(response);
+
         })
-
-
 
         .catch(function (error) {
             console.error(error);
