40d4fae705230d307593ba8cf61b6c483658fcdd
JosephLindzius <joseph.lindzius@gmail.com>
2019-09-13 12:15:48 +0200

    shadows added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ function getRandomIndex(items) {
     return Math.floor(Math.random() * items.length);
 }
 
-
 function playAnimation () {
-    var nameBox = document.getElementById("name");
-    var deEvolvedBox = document.getElementById("de-evolved");
+    var searchWrapper = document.getElementById("searchWrapper");
+    var nameBox = document.getElementById("pokemonWrapper");
+    var deEvolvedBox = document.getElementById("deEWrapper");
     nameBox.animate([
         { opacity: '.9' },
         { opacity: '1' },
@@ -15,6 +15,14 @@ function playAnimation () {
         iterations: Infinity
     });
     deEvolvedBox.animate([
+        { opacity: '.9' },
+        { opacity: '1' },
+        { opacity: '.9' }
+    ], {
+        duration: 2000,
+        iterations: Infinity
+    });
+    searchWrapper.animate([
         { opacity: '.9' },
         { opacity: '1' },
         { opacity: '.9' }
